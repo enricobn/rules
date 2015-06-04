a8211fad8f464b175d491b46dbf388d0e6fbfaee
Enrico Benedetti <enricobn@gmail.com>
2015-06-04 23:44:45 +0200

    first test with Ajax forms

--- lift/src/main/scala/org/rules/lift/snippet/Index.scala
+++ lift/src/main/scala/org/rules/lift/snippet/Index.scala
@@ -3,7 +3,7 @@ package org.rules.lift.snippet
 import java.io.File
 
 import _root_.net.liftweb.http.SHtml._
-import net.liftweb.http.RequestVar
+import net.liftweb.http.{SHtml, RequestVar}
 import net.liftweb.http.js.JsCmd
 import org.rules.rule.{XMLRule, XMLModule, XMLProject}
 
@@ -54,19 +54,58 @@ class Ajax {
 
   private def updateRules(module: XMLModule) : JsCmd = {
     SetHtml("content",
-      <h2>Rules</h2> ++ module.rules.foldLeft(NodeSeq.Empty) {(actual,rule) => actual ++ ruleSeq(rule) ++ <br></br>}
+      <h2>Rules</h2> ++ module.rules.foldLeft(NodeSeq.Empty) {(actual,rule) => actual ++ ruleForm(rule) ++ <br></br>}
     )
   }
 
   private def ruleSeq(rule: XMLRule) : NodeSeq = {
     <h3>{rule.name}</h3> ++
-    <br></br> ++
     Text("requires: ") ++ Text(rule.requiresList.toString()) ++
     <br></br> ++
-    Text("provides: ") ++ Text(rule.provides.toString()) ++
+    Text("provides: ") ++ Text(rule.xmlProvides.toString()) ++
     <br></br> ++
     Text("runScript: ") ++ Text(rule.runScript.toString)
   }
+
+  def addRule(xhtml : NodeSeq) : NodeSeq = {
+    var requires = findNode(<entry:requires></entry:requires>, xhtml).head.\@("value")
+    var provides = findNode(<entry:provides></entry:provides>, xhtml).head.\@("value")
+    var runScript = findNode(<entry:runScript></entry:runScript>, xhtml).head.\@("value")
+
+    def processEntryAdd() = {
+      println(requires + "," + provides + "," + runScript)
+    }
+
+    bind("entry", xhtml,
+      "requires" -> SHtml.text(requires, requires = _),
+      "provides" -> SHtml.text(provides, provides = _),
+      "runScript" -> (SHtml.text(runScript, runScript = _) ++ SHtml.hidden(processEntryAdd))
+    )
+
+  }
+
+  private def ruleForm(rule: XMLRule) : NodeSeq = {
+    <h3>{rule.name}</h3> ++
+    <form class="lift:form.ajax">
+      <lift:Ajax.addRule>
+        requires: <entry:requires value={rule.requiresList.toString()}/><br />
+        provides: <entry:provides value={rule.provides.toString()}/><br />
+        runScript: <entry:runScript value={rule.runScript.toString}/><br />
+        <input type="submit" value="Submit" />
+      </lift:Ajax.addRule>
+    </form>
+  }
+
+
+/*  private def ruleForm(rule: XMLRule) : NodeSeq = {
+    <lift:Ajax.addRule form="POST">
+      requires: <entry:requires value={rule.requiresList.toString()}/><br />
+      provides: <entry:provides value={rule.provides.toString()}/><br />
+      runScript: <entry:runscript value={rule.runScript.toString}/><br />
+      <entry:submit />
+    </lift:Ajax.addRule>
+  }
+*/
 
   private def updateFactories(module: XMLModule) : JsCmd = {
     SetHtml("content",
@@ -95,20 +134,21 @@ object Index {
   def header =
     <h2>Rules</h2>
 
-  def nav =
-      <div>
+  def nav = Text("")
+/*      <div>
         London<br/>
         Paris<br/>
         Tokyo<br/>
       </div>
-
+*/
   def footer =
     <h3>(C) 2015 rules.org</h3>
 
-  def content =
-  Range(0,10).foldLeft(NodeSeq.Empty){ (actual,r) =>
+  def content = Text("")
+/*  Range(0,10).foldLeft(NodeSeq.Empty){ (actual,r) =>
     actual ++ <h1>London</h1>
       <p>London is the capital city of England. It is the most populous city in the United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
       <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
-  }
+      */
+
 }
